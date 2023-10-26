18878a1c217498459ab89db7238b80f11db7b504
Lazar <lazar.mrkic@sigmoid.rs>
2023-10-26 15:55:01 +0200

    add IP based rate limiting, current progress

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"golang.org/x/time/rate"
+	"net"
 	"net/http"
+	"sync"
 )
 
 // ukoliko se desi neki "panic", njega će automatski da uhvati "http.Server"
@@ -31,17 +33,44 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 	})
 }
 
+// "IP-based" rate limiter
+// svaki put kad određeni klijent napravi "request" ka API-ju, inicijalizovaće se novi "rate limiter" i biće dodat u mapu
+// za svaki prateći "request", izvadićemo klijentov "rate limiter" iz mape i provjeriti da li njegov "request" treba da se odobri
+// to se radi pozivom "Allow()" metode
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	// inicijalizuje se "rate limiter"
-	// on dozvoljava 2 "request"-a po sekundi sa maksimalno 4 "request"-a u jednom "burst"-u
-	limiter := rate.NewLimiter(2, 4)
+	var (
+		// mapa koja sadrži klijentske IP adrese i "rate limiter"-e
+		clients = make(map[string]*rate.Limiter)
+		// mapa kao struktura podataka nije sigurna za "concurrent use"
+		// "rateLimit()" middleware može da se izvršava u više "gouroutine"-a u isto vrijeme
+		// takođe, Golang-ov server za svaki HTTP "request" ima poseban "goroutine"
+		// zbog toga, preko "mutex"-a će se uvesti ograničenje - samo jedan "goroutine" može da čita vrijednosti iz mape ili da upisuje u nju
+		mu sync.Mutex
+	)
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !limiter.Allow() {
+		// vadi se klijentska IP adresa iz "request"-a
+		// "_" bi predstavljao "port" iz adrese
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+
+		//zaključavanje "mutex"-a, kako se ovaj kod ne bi izvšavao konkurentno
+		mu.Lock()
+
+		// provjera da li IP adresa postoji unutar mape
+		// ukoliko ne, inicijalizuje se novi "rate limiter" i dodaje se u mapu skupa sa povezanom IP adresom
+		if _, found := clients[ip]; !found {
+			clients[ip] = rate.NewLimiter(2, 4)
+		}
+
+		if !clients[ip].Allow() {
+			mu.Unlock()
 			app.rateLimitExceededResponse(w, r)
 			return
 		}
 
-		next.ServeHTTP(w, r)
 	})
 }
