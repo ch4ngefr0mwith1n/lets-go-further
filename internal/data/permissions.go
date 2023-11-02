ec2aa465cf3bdce9b10f7952842ffaf6fd7e46f2
Lazar <baldur059@gmail.com>
2023-11-02 23:12:07 +0100

    set up the permissions model

---
+++ internal/data/permissions.go
@@ -0,0 +1,65 @@
+package data
+
+import (
+	"context"
+	"database/sql"
+	"time"
+)
+
+// unutar "Permissions" slice-a će biti sadržani svi "permission" kodovi (recimo, "movies:read" i "movies:write")
+type Permissions []string
+
+// "PermissionModel" će služiti za interakciju sa tabelama "permissions" i "users_permissions"
+type PermissionModel struct {
+	DB *sql.DB
+}
+
+// metoda koja provjerava da li "Permissions" slice sadrži određeni "permission" kod:
+func (p Permissions) Include(code string) bool {
+	for i := range p {
+		if code == p[i] {
+			return true
+		}
+	}
+
+	return false
+}
+
+// metoda "GetAllPermissionsForUser" vraća sve "permission" kodove za određenog korisnika
+// oni će biti u okviru "Permissions" slice-a
+func (m PermissionModel) GetAllPermissionsForUser(userID int64) (Permissions, error) {
+	query := `
+        SELECT permissions.code
+        FROM permissions
+        INNER JOIN users_permissions ON users_permissions.permission_id = permissions.id
+        INNER JOIN users ON users_permissions.user_id = users.id
+        WHERE users.id = $1`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	rows, err := m.DB.QueryContext(ctx, query, userID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var permissions Permissions
+
+	for rows.Next() {
+		var permission string
+
+		err := rows.Scan(&permission)
+		if err != nil {
+			return nil, err
+		}
+
+		permissions = append(permissions, permission)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return permissions, nil
+}
